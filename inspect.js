89060ff32060e6e6a46b798d40eafdcc8fc1a7f2
Brave z <brave.z@a.kitchen>
2017-07-05 11:13:13 +0800

    test

--- inspect.js
+++ inspect.js
@@ -1,4 +1,14 @@
 function onClick(){
 	o = document.getElementById("out");
 	o.innerHTML = "Hi!";
+	
+	
+	let options = {};
+	options.acceptAllDevices = true;
+	navigator.bluetooth.requestDevice(options).then(device => {
+			console.log('Got device:', device.name); 
+			console.log('id:', device.id);  
+			console.log('Connected:' + device.gatt.connected)});
+
+
 }
