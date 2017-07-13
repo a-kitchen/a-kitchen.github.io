d21451e22f7c7b60a42f36d6cfc5607a0e745313
Brave z <brave.z@a.kitchen>
2017-07-13 12:06:09 +0800

    test

--- inspect.js
+++ inspect.js
@@ -3,6 +3,31 @@
  BluetoothRemoteGATTServer bltr;
   BluetoothRemoteGattCharacteristic crx,ctx;
 
+function search(){
+	document.getElementById("demo").innerHTML="My First JavaScript";
+	let options = {};
+	     let filters = [];
+	    let filterName = document.querySelector('#name').value;
+  if (filterName) {
+    filters.push({name: filterName});
+  }
+  options.filters = filters;
+	   
+	options.acceptAllDevices = true;
+	navigator.bluetooth.requestDevice(options).then(device => {
+			console.log('Got device:', device.name);
+			console.log('id:', device.id);
+			console.log('Connected:' + device.gatt.connected);
+			console.log('Connected:' + device.gatt.connected);
+			document.getElementById("demo").innerHTML="My First JavaScript"+device.name;
+		//	bltr =device.gatt.connect();
+		
+			}).catch(error => {
+			document.getElementById("demo").innerHTML="error"+error;
+			});
+	
+}
+
 
 //function getService(){
 	//blegatt = bltr.getPrimaryService('00000000-0000-1000-8000-00805F9B34FB');
